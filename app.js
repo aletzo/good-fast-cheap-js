11a8fb7e06f00ba80a662b1e3e8110d16d53713f
aletzo <ajo@mydevnull.net>
2017-05-18 12:21:12 +0200

    Add a request to the server

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
 let values = [];
 
-const toggle = (field) => {
-    const index = values.indexOf(field);
+const sendToServer = (values) => {
+    return values;
+}
 
-    if (index == -1) {
-        if (values.length == 2) {
-            const removedField = values.shift();
-            document.getElementById(removedField).checked = false;
+const toggle = (field) => {
+    setTimeout(() => {
+        const index = values.indexOf(field);
+
+        if (index == -1) {
+            if (values.length == 2) {
+                const removedField = values.shift();
+                document.getElementById(removedField).checked = false;
+            }
+
+            values.push(field);
+        } else {
+            values.splice(index, 1);
+
         }
 
-        values.push(field);
-    } else {
-        values.splice(index, 1);
+        const response = sendToServer(values);
 
-    }
+        const fields = [
+            'good',
+            'fast',
+            'cheap',
+        ];
+
+        fields.forEach(f => {
+            const isChecked = response.indexOf(f) != -1;
+            document.getElementById(f).checked = isChecked;
+        });
+        
+    }, 1000)
 }
 
